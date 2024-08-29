d70c7284cd0310b823aeb0f02a3bbc962a7b50b6
Molezov <hello@molezov.com>
2024-08-29 17:23:10 +0900

    feat: add Sentry integration

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -1,7 +1,20 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { json, Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData, useRouteError } from '@remix-run/react';
 import './tailwind.css';
+import { captureRemixErrorBoundaryError } from '@sentry/remix';
+
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+
+  captureRemixErrorBoundaryError(error);
+  return <>error</>;
+};
+
+export async function loader() {
+  return json({ variable: process.env.SENTRY_DSN });
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { variable } = useLoaderData<typeof loader>();
   return (
     <html lang="ja">
       <head>
@@ -9,6 +22,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script dangerouslySetInnerHTML={{ __html: `window.SENTRY_DSN = '${variable}'` }}></script>
       </head>
       <body>
         {children}
